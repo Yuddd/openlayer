0d71728420905ca14702f1c3520922597143b4c9
Yuddd <13270236516@163.com>
2019-07-18 14:35:36 +0800

    test

---
+++ src/CommonVariable.js
@@ -0,0 +1,11 @@
+/** set map
+ * para
+ * var mapSettiings = $.extend({
+        mapSouce: new ol.source.OSM(),
+        mapCenter: [0, 0],
+        mapZoom: 2
+    }, mapOptions);
+ **/
+var mapOptions = {
+    mapTarget: 'map'
+}
